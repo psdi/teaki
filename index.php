2b25d5fb2d726ac277590b20e96e11af99130a10
Psalm Diao <p.diao25@gmail.com>
2025-08-23 17:07:20 +0200

    Inject dependencies using container and environment variables

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 
 require_once 'vendor/autoload.php';
 
+(new Symfony\Component\Dotenv\Dotenv)->load(__DIR__ . '/config/.env');
+
+$builder = new \DI\ContainerBuilder;
+$builder->useAutowiring(false);
+$builder->useAttributes(false);
+$builder->addDefinitions('config/container.php');
+$container = $builder->build();
+
 $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
 );
 
 $router = (require_once 'config/routes.php')(new League\Route\Router);
-
+$strategy = new League\Route\Strategy\ApplicationStrategy;
+$strategy->setContainer($container);
+$router->setStrategy($strategy);
 $response = $router->dispatch($request);
 
 // send the response to the browser
